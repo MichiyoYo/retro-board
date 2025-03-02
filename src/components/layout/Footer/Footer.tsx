43c2488aca12008d2d4e1a87695a3fc57fdd9c77
Cristina Lester <cristina.deelester@gmail.com>
2025-03-02 07:53:24 -0800

    added footer elements

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,1 +1,26 @@
-export const Footer = () => <footer className='flex flex-none'>footer</footer>;
+import { Icon } from '@/ui';
+
+export const Footer = () => (
+  <footer className='flex items-center flex-none justify-between'>
+    <p className='text-xs flex gap-1'>
+      Designed and coded with <Icon name='heart' size={16} /> by{' '}
+      <a href='https://cristinalester.rocks' target='_blank'>
+        <span className='underline'>Cristina Lester</span>
+      </a>
+    </p>
+    <div className='flex gap-2 align-center'>
+      <a href='https://github.com/MichiyoYo' target='_blank'>
+        <Icon name='github' size={24} />
+      </a>
+      <a
+        href='https://www.linkedin.com/in/cristinalesterrocks/'
+        target='_blank'
+      >
+        <Icon name='linkedin' size={24} />
+      </a>
+      <a href='https://github.com/MichiyoYo' target='_blank'>
+        <Icon name='instagram-detailed' size={24} />
+      </a>
+    </div>
+  </footer>
+);
--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -2,6 +2,9 @@ import { Icon } from '@/ui';
 import { Button } from 'pixel-retroui';
 
 export const Header = () => {
+  const handleBuyMeACoffee = () => {
+    window.open('https://buymeacoffee.com/creechan', '_blank');
+  };
   return (
     <header className='flex justify-between w-full items-center flex-none'>
       <div className='flex-none'>
@@ -12,8 +15,12 @@ export const Header = () => {
       </div>
       <h1 className='text-2xl flex-1 text-center'>Retro Board</h1>
       <div className='flex-none '>
-        <Button className='flex items-center gap-1 invert ' bg='#a1a1a12b'>
-          <Icon name='coffee' size={24} />
+        <Button
+          className='flex items-center gap-1 invert'
+          bg='#a1a1a12b'
+          onClick={handleBuyMeACoffee}
+        >
+          <Icon name='coffee-takeaway' size={16} />
           <span className='text-xs'>buy me a coffee</span>
         </Button>
       </div>

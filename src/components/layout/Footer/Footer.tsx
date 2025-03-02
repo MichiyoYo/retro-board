9d0ec10af29ab8c732ec8933d4b645176cb0beb4
Cristina Lester <cristina.deelester@gmail.com>
2025-03-02 08:34:49 -0800

    added notion link

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -8,19 +8,32 @@ export const Footer = () => (
         <span className='underline'>Cristina Lester</span>
       </a>
     </p>
-    <div className='flex gap-2 align-center'>
-      <a href='https://github.com/MichiyoYo' target='_blank'>
-        <Icon name='github' size={24} />
-      </a>
-      <a
-        href='https://www.linkedin.com/in/cristinalesterrocks/'
-        target='_blank'
-      >
-        <Icon name='linkedin' size={24} />
-      </a>
-      <a href='https://github.com/MichiyoYo' target='_blank'>
-        <Icon name='instagram-detailed' size={24} />
-      </a>
+    <div className=''>
+      <ul className='flex gap-1 align-center'>
+        <li>
+          <a href='https://github.com/MichiyoYo' target='_blank'>
+            <Icon name='github' size={24} />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.linkedin.com/in/cristinalesterrocks/'
+            target='_blank'
+          >
+            <Icon name='linkedin' size={24} />
+          </a>
+        </li>
+        <li>
+          <a href='https://www.instagram.com/kitten_surimi/' target='_blank'>
+            <Icon name='instagram-detailed' size={24} />
+          </a>
+        </li>
+        <li className='mt-1'>
+          <a href='https://www.notion.com/@purplestuffcrafts' target='_blank'>
+            <Icon name='notion' size={18} />
+          </a>
+        </li>
+      </ul>
     </div>
   </footer>
 );
